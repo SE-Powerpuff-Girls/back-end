b2991e00857c05ad40ab4ea5c09b931466f7781a
TheXGeneral <ariondan01@gmail.com>
2022-04-22 17:30:18 +0300

    Refactor DB file for Heroku

--- database.js
+++ database.js
@@ -7,7 +7,10 @@ const isProduction = process.env.NODE.NODE_DEV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: connectionString,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        //rejectUnauthorized: false,
+    },
 });
 
 module.exports = { pool };
