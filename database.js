dc89e2a12c3e700a8de81569a490e11a6040f08a
TheXGeneral <ariondan01@gmail.com>
2022-05-23 20:02:04 +0300

    One more time

--- database.js
+++ database.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_DEV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 	ssl: {
 		rejectUnauthorized: false,
 	},
