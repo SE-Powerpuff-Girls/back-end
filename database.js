b28484fbd71ac95b3afd2906b9d2b77471a7c71e
TheXGeneral <ariondan01@gmail.com>
2022-05-23 19:55:32 +0300

    One more time

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE.NODE_DEV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 	ssl: {
 		rejectUnauthorized: false,
 	},
